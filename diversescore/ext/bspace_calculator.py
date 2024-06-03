ef32409dc0e5be7cc67318f4838a344c4d832069
MFaisalZaki <mustafa.faisal.eng@gmail.com>
2024-06-03 23:44:30 +0100

    removing the old bspace_calc.

--- diversescore/ext/bspace_calculator.py
+++ diversescore/ext/bspace_calculator.py
@@ -5,55 +5,56 @@ from unified_planning.io import PDDLReader
 from unified_planning.shortcuts import *
 
 from diversescore.utilities import createActionDictFromTask, constructSequentialPlanFromActionDict
-from bspace.shortcuts import *
+# from bspace.shortcuts import *
 
 def BSpaceCalculator(domain, problem, planset, task, bspace_cfg, **kwargs):
-    bspacecfg = json.load(open(bspace_cfg, 'r'))
-    configurations = defaultdict(dict)
-    configurations['balance-behaviours'] = eval(bspacecfg['balance-behaviours'])
-    configurations['dims'] = []
+    pass
+    # bspacecfg = json.load(open(bspace_cfg, 'r'))
+    # configurations = defaultdict(dict)
+    # configurations['balance-behaviours'] = eval(bspacecfg['balance-behaviours'])
+    # configurations['dims'] = []
     
-    if eval(bspacecfg.get('include-objs-count-dim', 'False')) and task['resource-details'] is not None:
-        configurations['dims'].append(ResourceCount(parsed_resources=task['resource-details']['objects']))
+    # if eval(bspacecfg.get('include-objs-count-dim', 'False')) and task['resource-details'] is not None:
+    #     configurations['dims'].append(ResourceCount(parsed_resources=task['resource-details']['objects']))
 
-    if eval(bspacecfg.get('include-plan-length-dim', 'False')):
-        configurations['dims'].append(PlanLength(forceNoActionAfterGoal=False))
+    # if eval(bspacecfg.get('include-plan-length-dim', 'False')):
+    #     configurations['dims'].append(PlanLength(forceNoActionAfterGoal=False))
 
-    if eval(bspacecfg.get('include-goal-ordering-dim', 'False')):
-        configurations['dims'].append(GoalOrderingPredicates())
+    # if eval(bspacecfg.get('include-goal-ordering-dim', 'False')):
+    #     configurations['dims'].append(GoalOrderingPredicates())
     
-    if eval(bspacecfg.get('include-objs-utils-dim', 'False')) and task['resource-details'] is not None:
-        if len(task['resource-details']['objects']) > 0:
-            configurations['dims'].append(Objects(parsed_resources=task['resource-details']['objects']))
+    # if eval(bspacecfg.get('include-objs-utils-dim', 'False')) and task['resource-details'] is not None:
+    #     if len(task['resource-details']['objects']) > 0:
+    #         configurations['dims'].append(Objects(parsed_resources=task['resource-details']['objects']))
     
-    if eval(bspacecfg.get('include-fns-utils-dim', 'False')) and task['resource-details'] is not None:
-        if len(task['resource-details']['fns']) > 0:
-            configurations['dims'].append(Functions(parsed_resources=task['resource-details']['fns']))
+    # if eval(bspacecfg.get('include-fns-utils-dim', 'False')) and task['resource-details'] is not None:
+    #     if len(task['resource-details']['fns']) > 0:
+    #         configurations['dims'].append(Functions(parsed_resources=task['resource-details']['fns']))
 
-    planningtask = PDDLReader().parse_problem(domain, problem)
-    _bspace = BehaviourSpace(planningtask, configurations)
-    formulalength = max(set([len(plan)-1 for plan in planset]))
+    # planningtask = PDDLReader().parse_problem(domain, problem)
+    # _bspace = BehaviourSpace(planningtask, configurations)
+    # formulalength = max(set([len(plan)-1 for plan in planset]))
 
-    # to reduce the construction time for the planset from txt to SequentialPlan,
-    # we will use a dict the maps the action name to 
+    # # to reduce the construction time for the planset from txt to SequentialPlan,
+    # # we will use a dict the maps the action name to 
 
-    actiondictmap = createActionDictFromTask(_bspace.encoder.encoder.ground_problem)
-    planset = [constructSequentialPlanFromActionDict(plan, actiondictmap, _bspace.encoder.encoder.ground_problem) for plan in planset]
+    # actiondictmap = createActionDictFromTask(_bspace.encoder.encoder.ground_problem)
+    # planset = [constructSequentialPlanFromActionDict(plan, actiondictmap, _bspace.encoder.encoder.ground_problem) for plan in planset]
     
-    _, _, _ = _bspace.encode(planset[0], formulalength)
+    # _, _, _ = _bspace.encode(planset[0], formulalength)
 
-    optimise_behaviour_count = kwargs.get('optimise_behaviour_count', False)
-    selected_k_plans = kwargs.get('selected_k_plans', 0)
+    # optimise_behaviour_count = kwargs.get('optimise_behaviour_count', False)
+    # selected_k_plans = kwargs.get('selected_k_plans', 0)
 
-    if optimise_behaviour_count:
-        # In this mode we need to keep appending plan by plan and check the behaviour count.
-        for idx, plan in enumerate(planset[1:]):
-            _bspace.append(plan, i=idx, run_plan_validation=False, return_bplan=False)
-            if _bspace.behavioursCountScore() >= selected_k_plans: break
-    else:
-        _bspace.extend(planset[1:], skip_sat_test=True)
+    # if optimise_behaviour_count:
+    #     # In this mode we need to keep appending plan by plan and check the behaviour count.
+    #     for idx, plan in enumerate(planset[1:]):
+    #         _bspace.append(plan, i=idx, run_plan_validation=False, return_bplan=False)
+    #         if _bspace.behavioursCountScore() >= selected_k_plans: break
+    # else:
+    #     _bspace.extend(planset[1:], skip_sat_test=True)
 
-    retscores = defaultdict(dict)
-    retscores['behaviours-count'] = _bspace.behavioursCountScore()
-    retscores['dimensions-count'] = _bspace.dimensionsCountScore()
-    return retscores
+    # retscores = defaultdict(dict)
+    # retscores['behaviours-count'] = _bspace.behavioursCountScore()
+    # retscores['dimensions-count'] = _bspace.dimensionsCountScore()
+    # return retscores
